1a7126779a8ee227e2ed74c6c15c0c371538eafd
Stanley Shi <stanleyshi42@gmail.com>
2022-10-13 11:39:08 -0700

    removed some comments

--- src/com/cognixia/jump/myshowlist/DAOClass.java
+++ src/com/cognixia/jump/myshowlist/DAOClass.java
@@ -18,10 +18,7 @@ public class DAOClass implements DAO {
 		try {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM Shows");
-			
 			List<Show> showList = new ArrayList<Show>();
-			
-			//rs.first();
 			
 			while(rs.next()) {
 				int id = rs.getInt("showID");
@@ -42,35 +39,25 @@ public class DAOClass implements DAO {
 	@Override
 	public List<Tracker> getAllTrackers() {
 		try {
-			// find all the departments...
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM Trackers");
-			
 			List<Tracker> trackerList = new ArrayList<Tracker>();
 			
-			//rs.first();
-			
 			while(rs.next()) {
-				// ...iterate through to get column info...
 				int userID = rs.getInt("user_id");
 				int showID = rs.getInt("show_id");
 				int currentEpisode = rs.getInt("current_episode");
 				int currentSeason = rs.getInt("current_season");
 				int statusID = rs.getInt("s_id");
 				
-				
-				// ...then add them to a list...
 				Tracker trak = new Tracker(userID, showID, currentEpisode, currentSeason, statusID);
 				trackerList.add(trak);
 			}
-			
-			// ...and return that list once finished
 			return trackerList;
 			
 		} catch (SQLException e) {
 			System.out.println("Could not retrieve list of trackers from database");
 		}
-		// return null just in case exception is thrown
 		return null;
 	}
 
@@ -190,13 +177,11 @@ public class DAOClass implements DAO {
 			pstmt.setInt(5, trak.getStatusID());
 			
 			int i = pstmt.executeUpdate();
-			
 			if(i > 0) {
 				return true;
 			}
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -214,15 +199,12 @@ public class DAOClass implements DAO {
 			pstmt.setInt(2, trak.getShowID());
 			
 			int i = pstmt.executeUpdate();
-			
 			if(i > 0) {
 				return true;
 			}
-			
 		} catch (SQLException e) {
 			System.out.println("Tracker not found.");
 		}
-		
 		return false;
 	}
 	
@@ -237,22 +219,18 @@ public class DAOClass implements DAO {
 			pstmt.setInt(2, showID);
 			
 			int i = pstmt.executeUpdate();
-			
 			if(i > 0) {
 				return true;
 			}
-			
 		} catch (SQLException e) {
 			System.out.println("Tracker for userID - " + "\"" + userID + "\"" + "showID - " 
 					+ "\"" + showID + "\" not found.");
 		}
-		
 		return false;
 	}
 
 	@Override
 	public boolean updateTracker(Tracker trak) {
-		// TODO Auto-generated method stub
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(
 				"UPDATE Trackers " +
@@ -291,5 +269,4 @@ public class DAOClass implements DAO {
 		}
 		return null;
 	}
-
 }
