99d06ede981a5112a91d896cb0aba3d4f8460bc3
Stanley Shi <stanleyshi42@gmail.com>
2022-10-12 20:45:45 -0700

    trackers now display show titles

--- src/com/cognixia/jump/myshowlist/ConsoleMenu.java
+++ src/com/cognixia/jump/myshowlist/ConsoleMenu.java
@@ -85,16 +85,13 @@ public class ConsoleMenu {
 	
 	private void userMenu(Scanner sc) {
 		List<Tracker> trackers = db.getAllUserTrackers(sessionID);
+		System.out.println("==Progress Trackers==");
 		System.out.format("%33s%10s%10s%14s", "Show", "Episodes",
 				"Seasons", "Status");
 		for(Tracker t : trackers) {
 			System.out.println();
-			System.out.format("%33s%10d%10d%14s", db.getShowById(t.getShowID()), t.getCurrentEpisode(),
+			System.out.format("%33s%10d%10d%14s", db.getShowById(t.getShowID()).getTitle(), t.getCurrentEpisode(),
 					t.getCurrentSeason(), db.getStatus(t.getStatusID()));
-//			System.out.print(db.getShowById(t.getShowID()));
-//			System.out.print("Episode: " + t.getCurrentEpisode());
-//			System.out.print("Season: " + t.getCurrentSeason());
-//			System.out.print("Status: " + db.getStatus(t.getStatusID()));
 		}
 		
 		while(true){
